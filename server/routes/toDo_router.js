a78c8e348e77e8aeebfd093ec1bdbed659abba20
Kelly Danger <kellyanndanger@gmail.com>
2020-10-17 17:23:13 -0500

    pool query retrieving table from database

--- server/routes/toDo_router.js
+++ server/routes/toDo_router.js
@@ -5,7 +5,18 @@ const pool = require('../modules/pool');
 
 router.get('/', (req, res) => {
   console.log('in get');
-  res.sendStatus(200);
+  let queryText = `SELECT * FROM "to-do";`;
+  pool.query(queryText).then((result) => {
+    res.send(result.rows);
+  }).catch((error) => {
+    console.log('error in get', error);
+    res.sendStatus(500)  
+  });
+});
+
+router.post('/', (req, res) => {
+  console.log('in post');
+  res.sendStatus(200)  
 });
 
 
