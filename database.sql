8133efaeceeae0a6ccc1fd8ac5439cc7cb05e795
Kelly Danger <kellyanndanger@gmail.com>
2020-10-17 17:02:01 -0500

    files and server setup

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "to-do" (
+"id" serial primary key,
+"taskName" varchar(160),
+"completed" boolean default false );
+
+INSERT INTO "to-do" ("taskName") VALUES ('Weekend Challenge Homework');
+INSERT INTO "to-do" ("taskName") VALUES ('Dishes');
+INSERT INTO "to-do" ("taskName") VALUES ('Singing Practice');
+INSERT INTO "to-do" ("taskName") VALUES ('Get auto paint repair kit');
+INSERT INTO "to-do" ("taskName") VALUES ('Journal');
