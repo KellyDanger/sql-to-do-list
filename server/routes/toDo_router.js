875dbb44e09144a3c690941b41ebb547f2f3a28b
Kelly Danger <kellyanndanger@gmail.com>
2020-10-17 17:19:04 -0500

    get up and running on serverside

--- server/routes/toDo_router.js
+++ server/routes/toDo_router.js
@@ -3,4 +3,10 @@ const express = require ('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+router.get('/', (req, res) => {
+  console.log('in get');
+  res.sendStatus(200);
+});
+
+
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
-
 const port = 5000;
-
 const toDoRouter = require('./routes/toDo_router.js');
 
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use('/toDo', toDoRouter);
+
+
+
+
+
 
 app.listen(port, () => {
     console.log("up and running on port: ", port);
