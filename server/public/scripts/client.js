019dcd4ca5d217e1fe0e27e1ac7b93e2998e94d2
Kelly Danger <kellyanndanger@gmail.com>
2020-10-17 18:26:44 -0500

    working on get function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,5 +4,37 @@ $(document).ready(onReady);
 
 function onReady(){
   console.log('hello from JQ');
-  
+  getTasks();
 }//end onReady
+
+function getTasks(){
+  console.log('getting Tasks');
+  $.ajax({
+    method: 'GET',
+    url: '/toDo'
+  }).then(function(response) {
+    console.log(response);  
+    appendToDom(response);  
+  }).catch(function(error){
+    console.log('error in get', error);    
+  });
+}//end getTasks
+
+function appendToDom (array) {
+  let completedText = '';
+  for(let task of array) {
+    if(task.completed === true){
+      completedText = 'Yes! Good Job!'
+    } else {
+      completedText = 'No'
+    }
+    $('#taskList').append(`
+      <tr>
+        <td>${task.taskName}</td>
+        <td>${completedText}</td>
+        <td><button>Complete Task</button></td>
+        <td><button>Delete Task</button></td>
+      </tr>
+    `);
+  }
+}//end appendToDom
